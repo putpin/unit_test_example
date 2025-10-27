e932965333a9685256be8f0f0e4d0ef59f52980d
cuongpv_mb <cuongpv@intern.vn>
2025-10-27 16:03:14 +0700

    Add isEven function and corresponding tests in MathUtils

--- lib/math_utils.dart
+++ lib/math_utils.dart
@@ -2,4 +2,8 @@ abstract class MathUtils {
   static int add(int a, int b) {
     return a + b;
   }
+
+  static bool isEven(int number) {
+    return number % 2 == 0;
+  }
 }
--- test/unit_test/math_utils_test.dart
+++ test/unit_test/math_utils_test.dart
@@ -13,4 +13,26 @@ void main() {
     // Assert
     expect(result, 2);
   });
+
+  group('Test isEven function', () {
+    test('isEven should return true for even number', () {
+      expect(MathUtils.isEven(2), true);
+    });
+
+    test('isEven should return false for odd number', () {
+      expect(MathUtils.isEven(3), false);
+    });
+
+    test('isEven should return true for zero', () {
+      expect(MathUtils.isEven(0), true); // Matcher isTrue
+    });
+
+    test('isEven should return true for negative even number', () {
+      expect(MathUtils.isEven(-4), isTrue); // Matcher isTrue
+    });
+
+    test('isEven should return false for negative odd number', () {
+      expect(MathUtils.isEven(-5), isFalse); // Matcher isFalse
+    });
+  });
 }
